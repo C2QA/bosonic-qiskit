21f3cecfbdda68c86bf9f1ae8f60339c24eaaa81
Tim Stavenger <timothy.stavenger@pnnl.gov>
2020-10-26 19:31:08 -0700

    make conditional gates subcircuits

--- c2qa/circuit.py
+++ c2qa/circuit.py
@@ -7,8 +7,8 @@ import numpy
 
 
 class CVCircuit(QuantumCircuit):
-    def __init__(self, qmr: QumodeRegister, qr: QuantumRegister, cr: ClassicalRegister):
-        super().__init__(qmr.qreg, qr, cr)
+    def __init__(self, qmr: QumodeRegister, qr: QuantumRegister, cr: ClassicalRegister, name: str = None):
+        super().__init__(qmr.qreg, qr, cr, name = name)
 
         self.qmr = qmr
         self.qr = qr
@@ -28,6 +28,15 @@ class CVCircuit(QuantumCircuit):
 
             super().initialize(vector, self.qmr[qumode])
 
+    def cv_conditional(self, name, op_a, op_b):
+        sub_qmr = QumodeRegister(self.qmr.num_qumodes, self.qmr.num_qubits_per_mode)
+        sub_qr = QuantumRegister(1)
+        sub_circ = QuantumCircuit(sub_qmr.qreg, sub_qr, name = name)
+        sub_circ.append(UnitaryGate(op_a).control(num_ctrl_qubits = 1, ctrl_state = 0), [sub_qr[0]] + sub_qmr[0])
+        sub_circ.append(UnitaryGate(op_b).control(num_ctrl_qubits = 1, ctrl_state = 1), [sub_qr[0]] + sub_qmr[1])
+
+        return sub_circ.to_instruction()        
+
     def cv_bs(self, phi, qumode_a, qumode_b):
         operator = self.ops.BS(phi)
         
@@ -40,8 +49,9 @@ class CVCircuit(QuantumCircuit):
         self.unitary(obj = operator, qubits = qumode, label = 'D')
 
     def cv_cnd_d(self, alpha, beta, ctrl, qumode_a, qumode_b):
-        self.append(UnitaryGate(self.ops.D(alpha)).control(num_ctrl_qubits = 1, label = 'Da', ctrl_state = 0), [ctrl] + qumode_a)
-        self.append(UnitaryGate(self.ops.D(beta)).control(num_ctrl_qubits = 1, label = 'Db', ctrl_state = 1), [ctrl] + qumode_b)
+        self.append(self.cv_conditional('Dc', self.ops.D(alpha), self.ops.D(beta)), [ctrl] + qumode_a + qumode_b)
+        # self.append(UnitaryGate(self.ops.D(alpha)).control(num_ctrl_qubits = 1, label = 'Da', ctrl_state = 0), [ctrl] + qumode_a)
+        # self.append(UnitaryGate(self.ops.D(beta)).control(num_ctrl_qubits = 1, label = 'Db', ctrl_state = 1), [ctrl] + qumode_b)
 
     def cv_r(self, phi, qumode):
         operator = self.ops.R(phi)
@@ -54,8 +64,9 @@ class CVCircuit(QuantumCircuit):
         self.unitary(obj = operator, qubits = qumode, label = 'S')
 
     def cv_cnd_s(self, z_a, z_b, ctrl, qumode_a, qumode_b):
-        self.append(UnitaryGate(self.ops.S(z_a)).control(num_ctrl_qubits = 1, label = 'Sa', ctrl_state = 0), [ctrl] + qumode_a)
-        self.append(UnitaryGate(self.ops.S(z_b)).control(num_ctrl_qubits = 1, label = 'Sb', ctrl_state = 1), [ctrl] + qumode_b)
+        self.append(self.cv_conditional('Sc', self.ops.S(z_a), self.ops.S(z_b)), [ctrl] + qumode_a + qumode_b)
+        # self.append(UnitaryGate(self.ops.S(z_a)).control(num_ctrl_qubits = 1, label = 'Sa', ctrl_state = 0), [ctrl] + qumode_a)
+        # self.append(UnitaryGate(self.ops.S(z_b)).control(num_ctrl_qubits = 1, label = 'Sb', ctrl_state = 1), [ctrl] + qumode_b)
 
     def cv_s2(self, z, qumode_a, qumode_b):
         operator = self.ops.S2(z)
