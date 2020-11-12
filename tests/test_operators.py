6a4195f64c846c3fa61a18b5b2791e92cd672a8b
stav405 <Timothy.Stavenger@pnnl.gov>
2020-11-12 08:14:57 -0800

    add tests of operator matrices

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,7 +1,8 @@
 import c2qa
+import numpy
 from c2qa.operators import CVOperators
 from qiskit.quantum_info.operators.predicates import is_unitary_matrix
-
+import random
 
 class TestUnitary:
 
@@ -23,3 +24,33 @@ class TestUnitary:
 
     def test_s2(self):
         assert is_unitary_matrix(self.ops.s2(1))
+
+
+class TestMatrices:
+    """Test that the operators produce the values we expect.
+    TODO - would be better to test against known input & output values vs simply non-zero
+    """
+
+    def setup_method(self, method):
+        qmr = c2qa.QumodeRegister(1, 1)
+        self.ops = CVOperators(qmr)
+
+    def test_bs(self):
+        op = self.ops.bs(random.random())
+        assert numpy.count_nonzero(op)
+
+    def test_d(self):
+        op = self.ops.d(random.random())
+        assert numpy.count_nonzero(op)
+
+    def test_r(self):
+        op = self.ops.r(random.random())
+        assert numpy.count_nonzero(op)
+
+    def test_s(self):
+        op = self.ops.s(random.random())
+        assert numpy.count_nonzero(op)
+
+    def test_s2(self):
+        op = self.ops.s2(random.random())
+        assert numpy.count_nonzero(op)
