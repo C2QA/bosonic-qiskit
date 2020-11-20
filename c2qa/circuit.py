9b639a5c91a3aa28c747e2cb8e6256730952da37
stav405 <Timothy.Stavenger@pnnl.gov>
2020-11-20 11:48:10 -0800

    conditional gates are on a single qumode

--- c2qa/circuit.py
+++ c2qa/circuit.py
@@ -47,11 +47,11 @@ class CVCircuit(QuantumCircuit):
 
     def cv_conditional(self, name, op_a, op_b):
         """ Make two operators conditional (i.e., controlled by qubit in either the 0 or 1 state) """
-        sub_qmr = QumodeRegister(self.qmr.num_qumodes, self.qmr.num_qubits_per_mode)
+        sub_qmr = QumodeRegister(1, self.qmr.num_qubits_per_mode)
         sub_qr = QuantumRegister(1)
         sub_circ = QuantumCircuit(sub_qmr.qreg, sub_qr, name=name)
         sub_circ.append(UnitaryGate(op_a).control(num_ctrl_qubits=1, ctrl_state=0), [sub_qr[0]] + sub_qmr[0])
-        sub_circ.append(UnitaryGate(op_b).control(num_ctrl_qubits=1, ctrl_state=1), [sub_qr[0]] + sub_qmr[1])
+        sub_circ.append(UnitaryGate(op_b).control(num_ctrl_qubits=1, ctrl_state=1), [sub_qr[0]] + sub_qmr[0])
 
         return sub_circ.to_instruction()
 
@@ -65,8 +65,8 @@ class CVCircuit(QuantumCircuit):
 
         self.unitary(obj=operator, qubits=qumode, label='D')
 
-    def cv_cnd_d(self, alpha, beta, ctrl, qumode_a, qumode_b):
-        self.append(self.cv_conditional('Dc', self.ops.d(alpha), self.ops.d(beta)), [ctrl] + qumode_a + qumode_b)
+    def cv_cnd_d(self, alpha, beta, ctrl, qumode_a):
+        self.append(self.cv_conditional('Dc', self.ops.d(alpha), self.ops.d(beta)), [ctrl] + qumode_a)
 
     def cv_r(self, phi, qumode):
         operator = self.ops.r(phi)
@@ -78,8 +78,8 @@ class CVCircuit(QuantumCircuit):
 
         self.unitary(obj=operator, qubits=qumode, label='S')
 
-    def cv_cnd_s(self, z_a, z_b, ctrl, qumode_a, qumode_b):
-        self.append(self.cv_conditional('Sc', self.ops.s(z_a), self.ops.s(z_b)), [ctrl] + qumode_a + qumode_b)
+    def cv_cnd_s(self, z_a, z_b, ctrl, qumode_a):
+        self.append(self.cv_conditional('Sc', self.ops.s(z_a), self.ops.s(z_b)), [ctrl] + qumode_a)
 
     def cv_s2(self, z, qumode_a, qumode_b):
         operator = self.ops.s2(z)
