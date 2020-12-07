bc0ca525002f2ce2829c61c2a811056548229921
stav405 <Timothy.Stavenger@pnnl.gov>
2020-12-07 10:55:18 -0800

    add tests to view matrix values across OS

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -40,11 +40,29 @@ class TestMatrices:
 
         assert not numpy.allclose(one, rand)
 
+    def test_bs_across_os(self, capsys):
+        """Doesn't actually test anything, but as it is run across platforms by GitHub Actions a manual comparison can be made between Linux, MacOS, and Windows"""
+        with capsys.disabled():
+            op = self.ops.bs(numpy.pi / 4)
+            print()
+            print(op)
+
+            assert len(op)
+
     def test_d(self):
         one = self.ops.d(1)
         rand = self.ops.d(random.random())
 
         assert not numpy.allclose(one, rand)
+
+    def test_d_across_os(self, capsys):
+        """Doesn't actually test anything, but as it is run across platforms by GitHub Actions a manual comparison can be made between Linux, MacOS, and Windows"""
+        with capsys.disabled():
+            op = self.ops.d(numpy.pi / 2)
+            print()
+            print(op)
+
+            assert len(op)
 
     def test_r(self):
         one = self.ops.r(1)
