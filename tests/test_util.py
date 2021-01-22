07b149721d6d55dc820e4c2fe91bc2249cb8f995
stav405 <Timothy.Stavenger@pnnl.gov>
2021-01-22 07:00:32 -0800

    trace zero and one Fock states

--- tests/test_util.py
+++ tests/test_util.py
@@ -37,7 +37,8 @@ def test_plot_one(capsys):
         print(state)
         c2qa.util.plot_wigner_fock_state(circuit, state, file="tests/one.png")
 
-def test_partial_trace(capsys):
+
+def test_partial_trace_zero(capsys):
     with capsys.disabled():
         qmr = c2qa.QumodeRegister(num_qumodes=1, num_qubits_per_mode=2)
         qr = qiskit.QuantumRegister(size=1)
@@ -47,21 +48,21 @@ def test_partial_trace(capsys):
         # qr[0] and cr[0] will init to zero
         circuit.cv_initialize(0, qmr[0])
 
-        alpha = numpy.sqrt(numpy.pi)
+        state = Statevector.from_instruction(circuit)
 
-        circuit.h(qr[0])
-        circuit.cv_cnd_d(alpha, -alpha, qr[0], qmr[0])
-        circuit.cv_d(1j * alpha, qmr[0])
-        circuit.cv_cnd_d(-alpha, alpha, qr[0], qmr[0])
-        circuit.cv_d(-1j * alpha, qmr[0])
-        circuit.h(qr[0])
-        circuit.measure(qr[0], cr[0])
+        print(c2qa.util.cv_partial_trace(circuit, state))
 
-        backend = qiskit.Aer.get_backend("statevector_simulator")
-        job = qiskit.execute(circuit, backend)
-        result = job.result()
 
-        state = result.get_statevector(circuit)
-        counts = result.get_counts(circuit)
+def test_partial_trace_one(capsys):
+    with capsys.disabled():
+        qmr = c2qa.QumodeRegister(num_qumodes=1, num_qubits_per_mode=2)
+        qr = qiskit.QuantumRegister(size=1)
+        cr = qiskit.ClassicalRegister(size=1)
+        circuit = c2qa.CVCircuit(qmr, qr, cr)
+
+        # qr[0] and cr[0] will init to zero
+        circuit.cv_initialize(1, qmr[0])
+
+        state = Statevector.from_instruction(circuit)
 
         print(c2qa.util.cv_partial_trace(circuit, state))
