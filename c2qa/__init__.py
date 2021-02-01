9cac6a75c834da529598f5392699c0caf66c2615
Tim Stavenger <timothy.stavenger@pnnl.gov>
2021-02-01 08:49:32 -0800

    capture warning messages as logs

--- c2qa/__init__.py
+++ c2qa/__init__.py
@@ -1,13 +1,17 @@
 """Top-level package for c2qa-qiskit."""
 # flake8: noqa
 
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 # Import order between QuTiP and QisKit is important, QuTiP fails if QisKit is imported first.
 import qutip
+
+# QisKit 0.23.4 uses deprecated np.bool in Numpy 1.20.0, this works around the warning messages
+# FIXME Remove once https://github.com/Qiskit/qiskit-terra/pull/5758 is incorporated into a release!
+logging.captureWarnings(True)
 
 from c2qa.circuit import CVCircuit
 from c2qa.qumoderegister import QumodeRegister
 
 import c2qa.util
-
-import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
