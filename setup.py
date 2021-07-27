7f55b977c1ba82777777a168fad9badad9889164
Tim Stavenger <Timothy.Stavenger@pnnl.gov>
2021-07-27 16:49:30 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='c2qa-qiskit',
+    version='0.0.1',
+    url='https://github.com/C2QA/c2qa-qiskit.git',
+    author='Tim Stavenger',
+    author_email='timothy.stavenger@pnnl.gov',
+    description='National Quantum Initiative Co-design Center for Quantum Advantage project to simulate hybrid bosonic-superconducting qubits within IBM QisKit',
+    packages=find_packages(),
+)
