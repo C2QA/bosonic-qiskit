bdb9702eb3a514447ee669bb510fcf3bdce32331
stav405 <Timothy.Stavenger@pnnl.gov>
2022-07-14 07:05:35 -0700

    match Ella's example

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -36,10 +36,20 @@ def test_with_classical():
     )
 
 def test_with_initialize():
-    qmr = c2qa.QumodeRegister(1, 1)
-    qbr = qiskit.QuantumRegister(1)
-    cbr = qiskit.ClassicalRegister(1)
+    number_of_modes = 5
+    number_of_qubits = number_of_modes
+    number_of_qubits_per_mode = 2
+    cutoff = 2 ** number_of_qubits_per_mode
+
+    qmr = c2qa.QumodeRegister(num_qumodes=number_of_modes, num_qubits_per_qumode=number_of_qubits_per_mode)
+    qbr = qiskit.QuantumRegister(size=number_of_qubits)
+    cbr = qiskit.ClassicalRegister(size=1)
     circuit = c2qa.CVCircuit(qmr, qbr, cbr)
+
+    sm = [0,0,1,0,0]
+    for i in range(qmr.num_qumodes):
+        circuit.cv_initialize(sm[i], qmr[i])
+
     circuit.initialize(numpy.array([0,1]), qbr[0])
 
     state, result = c2qa.util.simulate(circuit)
