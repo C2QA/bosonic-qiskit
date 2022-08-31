d0570baa1887c43c5efadfdde54d5c4c5d42a4fb
stav405 <Timothy.Stavenger@pnnl.gov>
2022-08-31 09:58:02 -0700

    use state.dim vs len(state)

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -238,7 +238,7 @@ def test_displacement_calibration(capsys):
         state = result.get_statevector(circuit)
         counts = result.get_counts(circuit)
 
-        assert len(state) > 0
+        assert state.dim > 0
         assert counts
 
         # print()
