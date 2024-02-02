8ad35c0aab3a3c71fa8f68fdeb62fa2c6833fffc
Tim Stavenger <timothy.stavenger@pnnl.gov>
2024-02-02 06:28:52 -0800

    correct setup.py for past upgrade to qiskit 0.44.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="timothy.stavenger@pnnl.gov",
     description="National Quantum Initiative Co-design Center for Quantum Advantage bosonic Qiskit simulator",
     long_description="Qiskit extension supporting simulation of bosonic qumode Fock states reprsented as qubits within Qiskit",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     packages=find_packages(),
-    install_requires=["qiskit==0.39", "matplotlib==3.5.2", "pylatexenc==2.10"],
+    install_requires=["qiskit==0.44.2", "qiskit_aer==0.12.2", "qiskit-ibm-runtime==0.12.2", "matplotlib==3.7.3", "pylatexenc==2.10"],
 )
