50cf52e2a30f7c092af3a8d5bdcc5dece75d58f5
Tim Stavenger <timothy.stavenger@pnnl.gov>
2024-02-19 12:12:25 -0800

    test upgrade to v1.0

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     long_description="Qiskit extension supporting simulation of bosonic qumode Fock states reprsented as qubits within Qiskit",
     python_requires=">=3.8",
     packages=find_packages(),
-    install_requires=["qiskit==0.44.2", "qiskit_aer==0.12.2", "qiskit-ibm-runtime==0.12.2", "matplotlib==3.7.3", "pylatexenc==2.10"],
+    install_requires=["qiskit==1.0.0", "qiskit_aer==0.13.3", "qiskit-ibm-runtime==0.19.1", "matplotlib==3.7.3", "pylatexenc==2.10"],
 )
