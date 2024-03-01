cf7f6facaadd1d9afdc642372dbfad227f9d07f5
Tim Stavenger <timothy.stavenger@pnnl.gov>
2024-03-01 07:38:45 -0800

    matplotlib 3.8.3 is not python 3.8 compatible

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     long_description="Qiskit extension supporting simulation of bosonic qumode Fock states reprsented as qubits within Qiskit",
     python_requires=">=3.8",
     packages=find_packages(),
-    install_requires=["qiskit==1.0.1", "qiskit_aer==0.13.3", "qiskit-ibm-runtime==0.20.0", "matplotlib==3.8.3", "pylatexenc==2.10"],
+    install_requires=["qiskit==1.0.1", "qiskit_aer==0.13.3", "qiskit-ibm-runtime==0.20.0", "matplotlib==3.7.3", "pylatexenc==2.10"],
 )
