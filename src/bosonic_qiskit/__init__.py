3002fb6c959448cc6cf5dbbd7b0bd455b3dd6f4f
Derek Tropf <derek.tropf@pnnl.gov>
2025-12-08 12:41:42 -0800

    Updating README and actions for package name change

--- src/bosonic_qiskit/__init__.py
+++ src/bosonic_qiskit/__init__.py
@@ -14,18 +14,6 @@ from . import (
     wigner,
 )
 
-import warnings
-
-# --- WARNING MESSAGE START ---
-warnings.warn(
-    "The 'c2qa' package has been deprecated and will be renamed "
-    "to 'bosonic-qiskit' in a future release of version 15.0. "
-    "Imports will need to be modified to `bosonic_qiskit`. Please update your imports accordingly. ",
-    DeprecationWarning,
-    stacklevel=2,  # This points the warning to the line that imports the package
-)
-# --- WARNING MESSAGE END ---
-
 __all__ = [
     "CVCircuit",
     "QumodeRegister",
